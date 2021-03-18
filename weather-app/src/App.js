d4da26716c28a4c9a7f8a06af720c318a39381d6
Jelle Vandevyvere <jelle.vandevyvere@student.hogent.be>
2021-03-18 20:26:57 +0100

    first commit :star

---
+++ weather-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Weather App</h1>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        made with love
+      </footer>
+    </div>
+  );
+}
+
+export default App;
